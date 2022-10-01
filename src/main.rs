9c099e30496ee7e1194438759e97090a2af1b917
mewanthahayesh <bimalkeeth@gmail.com>
2022-10-02 00:46:15 +1000

    reading line from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+
 fn main() {
+
+    let mut input=String::new();
+
+    println!("say something");
+    match io::stdin().read_line(&mut input) {
+        Ok(_)=>{
+            println!("you said {}",input)
+        },
+        Err(e)=>{
+            println!("something went wrong {}",e);
+        }
+    }
+
     println!("Hello, world!");
 }
